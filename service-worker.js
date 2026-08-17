803939cc2d82be45754b430f1c554b4a314a3aa4
kikecw <53857528+kikecw@users.noreply.github.com>
2026-08-17 15:20:35 +0200

    refresh pwa cache for login fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kidbehaviour-v10';
+const CACHE='kidbehaviour-v11';
 const ASSETS=['./','./index.html','./styles.css','./settings.css','./app.js','./settings.js','./cloud.js','./manifest.webmanifest','./apple-touch-icon-fixed.png','./icon-192-fixed.png','./icon-512-fixed.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
